5430e71fcd3191457bbbee85dd7726d94e6b4cae
uosdevelopers <uosdevelopers@gmail.com>
2023-06-25 16:40:23 +0500

    Initial Commit

---
+++ app/config/config.env.php
@@ -0,0 +1,20 @@
+<?php
+// DB Params
+define('DB_HOST', 'HOST');
+define('DB_USER', 'USERNAME');
+define('DB_PASS', 'PASSWORD');
+define('DB_NAME', 'DB NAME');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'URL');
+// Site Name
+define('SITENAME', 'InscripChat');
+
+// PHPMailer
+define("SMTP_HOST", "SMTP HOST");
+define("SMTP_PORT", "PORT");
+define("SMTP_USERNAME", "SMTP USERNAME");
+define("SMTP_PASSWORD", "SMTP PASSWORD");
+define("SMTP_NAME", "Inscripchat");
